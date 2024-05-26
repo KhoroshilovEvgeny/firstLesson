a411e95ceb2924cc50ba3e3282c58112f0717d55
KhoroshilovEvgeny <ds-khoroshilov@yandex.ru>
2024-05-26 22:31:17 +0700

    Выполнено задание №3 (ред 001)

--- src/Main.java
+++ src/Main.java
@@ -49,5 +49,74 @@ public class Main {
         // Многократный вызов lastNumSum . Последовательное сложение пяти чисел: 5, 11, 123, 14, 1,
         System.out.println(Utils.lastNumSum(Utils.lastNumSum(Utils.lastNumSum(Utils.lastNumSum(5, 11),
                 123), 14), 1));
+
+
+        // Домашнее задание №3
+        // 3.1 Модуль числа
+        System.out.println("Модуль числа -31 равен " + Utils.abs(-31));
+        System.out.println("Модуль числа 33 равен " + Utils.abs(33));
+
+        // 3.2 Безопасное деление
+        System.out.println("5 / 0 = " + Utils.safeDiv(5, 0));
+        System.out.println("8 / 2 = " + Utils.safeDiv(8, 2));
+
+        // 3.3 Максимум
+        System.out.println("Максимимум из двух чисесл 5 и 7 равен " + Utils.max(5, 7));
+        System.out.println("Максимимум из двух чисесл 8 и -1 равен " + Utils.max(8, -1));
+        System.out.println("Максимимум из двух чисесл 3 и 3 равен " + Utils.max(3, 3));
+
+        // 3.4 Строка сравнения
+        System.out.println(Utils.makeDecision(5, 7));
+        System.out.println(Utils.makeDecision(8, -1));
+        System.out.println(Utils.makeDecision(4, 4));
+
+        // 3.5 Тройной максимум
+        System.out.println("Максимимум из трех чисесл 5, 7 и 7 равен " + Utils.max3(5, 7, 7));
+        System.out.println("Максимимум из трех чисесл 8, -1 и 4 равен " + Utils.max3(8, -1, 4));
+        System.out.println("Максимимум из трех чисесл 3, 3 и 3 равен " + Utils.max3(3, 3, 3));
+
+        // 3.6 Тройная сумма
+        System.out.println("Два любых из трех чисел (5, 7, 2) дают в сумме третье число - "
+                + Utils.sum3(5, 7, 2));
+        System.out.println("Два любых из трех чисел (8, -1, 4) дают в сумме третье число - "
+                + Utils.sum3(8, -1, 4));
+        System.out.println("Два любых из трех чисел (6, 3, 3) дают в сумме третье число - "
+                + Utils.sum3(6, 3, 3));
+        System.out.println("Два любых из трех чисел (6, 3, -3) дают в сумме третье число - "
+                + Utils.sum3(6, 3, -3));
+
+        // 3.7 Двойная сумма
+        System.out.println("Сумма двух чисел 5 и 7 с округлением до 20 при условии диапазона от 10 до 19 равно "
+                + Utils.sum2(5, 7));
+        System.out.println("Сумма двух чисел 8 и -1 с округлением до 20 при условии диапазона от 10 до 19 равно "
+                + Utils.sum2(8, -1));
+        System.out.println("Сумма двух чисел 22 и -12 с округлением до 20 при условии диапазона от 10 до 19 равно "
+                + Utils.sum2(22, -12));
+
+        // 3.8 Тридцать пять
+        System.out.println("Число 5 делиться либо только на 3, либо только на 5 - " + Utils.is35(5));
+        System.out.println("Число 8 делиться либо только на 3, либо только на 5 - " + Utils.is35(8));
+        System.out.println("Число 15 делиться либо только на 3, либо только на 5 - " + Utils.is35(15));
+
+        // 3.9 Волшебная шестерка
+        System.out.println("Магическая шестерка для чисел 5 и 7 - " + Utils.magic6(5, 7));
+        System.out.println("Магическая шестерка для чисел 8 и 2 - " + Utils.magic6(8, 2));
+        System.out.println("Магическая шестерка для чисел 1 и 6 - " + Utils.magic6(1, 6));
+
+        // 3.10 Возраст
+        System.out.println(Utils.age(5));
+        System.out.println(Utils.age(31));
+        System.out.println(Utils.age(44));
+
+        // 3.11 День недели
+        System.out.println("Указываем только один день недели с порядковым номером 5 - " + Utils.day(5));
+        System.out.println("Указываем только один день недели с порядковым номером 3 - " + Utils.day(3));
+        System.out.println("Указываем только один день недели с порядковым номером 6 - " + Utils.day(6));
+
+        // 3.12 Вывод дней недели
+        System.out.println("тест для выводя дней недели начиная с четверга и до конца недели");
+        Utils.printDays("четверг");
+        System.out.println("тест для выводя дней недели начиная с чг и до конца недели");
+        Utils.printDays("чг");
     }
 }
--- src/Utils.java
+++ src/Utils.java
@@ -49,4 +49,148 @@ public class Utils {
     public static int lastNumSum(int a, int b) {
         return (a % 10) + (b % 10);
     }
+
+    // Домашнее задание №3
+    // 3.1 Модуль числа
+    public static int abs(int x) {
+        if (x < 0) {
+            x *= -1;
+        }
+        return x;
+    }
+
+    // 3.2 Безопасное деление
+    public static int safeDiv(int x, int y) {
+        return y == 0 ? 0 : x / y;
+    }
+
+    // 3.3 Максимум
+    public static int max(int x, int y) {
+        return x > y ? x : y;
+    }
+
+    // 3.4 Строка сравнения
+    public static String makeDecision(int x, int y) {
+        String result;
+        if (x > y) {
+            result = " > ";
+        } else if (x < y) {
+            result = " < ";
+        } else {
+            result = " == ";
+        }
+        return x + result + y;
+    }
+
+    // 3.5 Тройной максимум
+    public static int max3(int x, int y, int z) {
+        int max;
+        if (x > y) {
+            max = x;
+        } else {
+            max = y;
+        }
+        if (z > max) {
+            max = z;
+        }
+        return max;
+    }
+
+    // 3.6 Тройная сумма
+    public static boolean sum3(int x, int y, int z) {
+        boolean result = false;
+        if (x + y == z) {
+            result = true;
+        }
+        if (x + z == y) {
+            result = true;
+        }
+        if (y + z == x) {
+            result = true;
+        }
+        return result;
+    }
+
+    // 3.7 Двойная сумма
+    public static int sum2(int x, int y) {
+        int sum = x + y;
+        if (sum >= 10 && sum <= 19) {
+            sum = 20;
+        }
+        return sum;
+    }
+
+    // 3.8 Тридцать пять
+    public static boolean is35(int x) {
+        boolean result = false;
+        if ((x % 3 == 0) && (x % 5 != 0)) {
+            result = true;
+        }
+        if ((x % 5 == 0) && (x % 3 != 0)) {
+            result = true;
+        }
+        return result;
+    }
+
+    // 3.9 Волшебная шестерка
+    public static boolean magic6(int x, int y) {
+        boolean result = false;
+        if (x == 6 || y == 6 || x + y == 6) {
+            result = true;
+        }
+        if (x - y == 6 || y - x == 6) {
+            result = true;
+        }
+        return result;
+    }
+
+    // 3.10 Возраст
+    public static String age(int x) {
+        String result = " лет";
+        if (x % 10 == 1 && x != 11) {
+            result = " год";
+        }
+        if ((x % 10 == 2 && x != 12) || (x % 10 == 3 && x != 13) || (x % 10 == 4 && x != 14)) {
+            result = " года";
+        }
+        return x + result;
+    }
+
+    // 3.11 День недели
+    public static String day(int x) {
+        String result = switch (x) {
+            case 1 -> "понедельник";
+            case 2 -> "вторник";
+            case 3 -> "среда";
+            case 4 -> "четверг";
+            case 5 -> "пятница";
+            case 6 -> "суббота";
+            case 7 -> "воскресенье";
+            default -> "это не день недели";
+        };
+        return result;
+    }
+
+    // 3.12 Вывод дней недели
+    public static void printDays(String x) {
+        switch (x) {
+            case "понедельник":
+                System.out.println("понедельник");
+            case "вторник":
+                System.out.println();
+            case "среда":
+                System.out.println("среда");
+            case "четверг":
+                System.out.println("четверг");
+            case "пятница":
+                System.out.println("пятница");
+            case "суббота":
+                System.out.println("суббота");
+            case "воскресенье":
+                System.out.println("воскресенье");
+                break;
+            default:
+                System.out.println("это не день недели");
+        }
+    }
 }
